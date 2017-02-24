847449da91493ebf59a4d43577e15cf8f944053a
mylind <mychhl@126.com>
2017-02-24 10:21:11 +0800

    httpRoot
    httpRoot

--- bluemix-settings.js
+++ bluemix-settings.js
@@ -42,6 +42,7 @@ var settings = module.exports = {
     // Move the admin UI
     httpAdminRoot: '/red',
     //httpAdminRoot: '/ui',
+    httpRoot: '/ui',
 
     // You can protect the user interface with a userid and password by using the following property
     // the password must be an md5 hash  eg.. 5f4dcc3b5aa765d61d8327deb882cf99 ('password')
